b42c8206cf136bf27d42aab1d04c1941a50f1e6c
emphillips <matt-phillips@live.com>
2019-12-16 18:57:26 -0600

    updated

--- sql/connection.js
+++ sql/connection.js
@@ -1,32 +1,32 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 
 class Connection {
-  constructor() {
-    if (!this.pool) {
-      console.log('creating mysql connection...')
+	constructor() {
+		if (!this.pool) {
+			console.log('creating mysql connection...');
 
-      const config = {
-        connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
-      }
+			const config = {
+				connectionLimit: 100,
+				host: '35.226.112.169',
+				user: 'root',
+				password: 'password1',
+				database: 'PLEASE_READ_ME_VVV'
+			};
 
-      if (process.env.NODE_ENV === 'production' && process.env.CLOUD_INSTANCE) {
-        console.log(`connect socket: ${process.env.CLOUD_INSTANCE}`)
-        config.socketPath = `/cloudsql/${process.env.CLOUD_INSTANCE}`
-      }
+			if (process.env.NODE_ENV === 'production' && process.env.CLOUD_INSTANCE) {
+				console.log(`connect socket: ${process.env.CLOUD_INSTANCE}`);
+				config.socketPath = `/cloudsql/${process.env.CLOUD_INSTANCE}`;
+			}
 
-      this.pool = mysql.createPool(config)
+			this.pool = mysql.createPool(config);
 
-      return this.pool
-    }
+			return this.pool;
+		}
 
-    return this.pool
-  }
+		return this.pool;
+	}
 }
 
-const instance = new Connection()
+const instance = new Connection();
 
 module.exports = instance;
